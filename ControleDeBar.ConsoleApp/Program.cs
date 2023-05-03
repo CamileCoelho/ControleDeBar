bc1448555d4bb8f1519377dde3648cff8bf68672
Camile Coelho <camilecoelho23@gmail.com>
2023-05-03 11:49:35 -0300

    Popodado campos para teste.

--- ControleDeBar.ConsoleApp/Program.cs
+++ ControleDeBar.ConsoleApp/Program.cs
@@ -107,15 +107,52 @@ namespace ControleDeBar.ConsoleApp
         {
 
             InformacoesPessoais dados = new("Camile", "(49) 99999-9999", "Rua XXXX", "333.333.333-22");
+            InformacoesPessoais dados2 = new("Tiago", "(49) 99999-9999", "Rua YYYY", "111.222.333-44");
+            InformacoesPessoais dados3= new("Rech", "(49) 99999-9999", "Rua ZZZZ", "333.222.333-22");
 
             Funcionario camile = new Funcionario(dados, "1111");
             repositorioFuncionario.Insert(camile);
+            Funcionario tiago = new Funcionario(dados2, "2222");
+            repositorioFuncionario.Insert(tiago);
+            Funcionario rech = new Funcionario(dados3, "3333");
+            repositorioFuncionario.Insert(rech);
+
             Garcon camileGarcon = new Garcon(camile);
             repositorioGarcon.Insert(camileGarcon);
+            Garcon tiagog = new Garcon(tiago);
+            repositorioGarcon.Insert(tiagog);
+            Garcon rechg = new Garcon(rech);
+            repositorioGarcon.Insert(rechg);
+
             Produto cerveja = new Produto("BRHAMA", "cerveja", 10);
             repositorioProduto.Insert(cerveja);
+            Produto cerveja2 = new Produto("ORIGINAL", "cerveja", 10);
+            repositorioProduto.Insert(cerveja2);
+            Produto cerveja3 = new Produto("BUD", "cerveja", 12);
+            repositorioProduto.Insert(cerveja3);
+            Produto batata = new Produto("BATATA", "porção 500g", 35);
+            repositorioProduto.Insert(batata);
+            Produto polenta = new Produto("POLENTA", "porção 500g", 40);
+            repositorioProduto.Insert(polenta);
+
             Mesa mesa01 = new Mesa(camileGarcon);
             repositorioMesa.Insert(mesa01);
+            Mesa mesa02 = new Mesa(camileGarcon);
+            repositorioMesa.Insert(mesa02);
+            Mesa mesa03 = new Mesa(camileGarcon);
+            repositorioMesa.Insert(mesa03);
+            Mesa mesa04 = new Mesa(tiagog);
+            repositorioMesa.Insert(mesa04);
+            Mesa mesa05 = new Mesa(tiagog);
+            repositorioMesa.Insert(mesa02);
+            Mesa mesa06 = new Mesa(tiagog);
+            repositorioMesa.Insert(mesa06);
+            Mesa mesa07 = new Mesa(rechg);
+            repositorioMesa.Insert(mesa07);
+            Mesa mesa08 = new Mesa(rechg);
+            repositorioMesa.Insert(mesa08);
+
+
         }
     }
 }
